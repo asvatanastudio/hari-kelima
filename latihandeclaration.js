cc69616c0b5cdc65350a27e1510d30d742397a33
asvatanastudio <asvatanaplayground@gmail.com>
2025-09-15 15:30:46 +0700

    latihan lanjutan

---
+++ latihandeclaration.js
@@ -0,0 +1,20 @@
+function Mhs(nama, energi) {
+    let mhs = {};
+    mhs.nama = nama;
+    mhs.energi = energi;
+
+    mhs.makan = function(porsi) {
+        this.energi += porsi;
+        console.log(`Halo ${this.nama}, selamat makan!`);
+    }
+
+    mhs.main = function(jam) {
+        this.energi -= jam;
+        console.log(`Halo ${this.nama}, selamat main!`);
+    }
+
+    return mhs;
+}
+
+let aku = Mhs('ade', 10);
+let kamu = Mhs('kamu', 10);
