d709ea3c893842a87af31d8b3c1a8c7abb0e6e5c
asvatanastudio <asvatanaplayground@gmail.com>
2025-09-15 09:22:08 +0700

    "latihan2"

--- latihan1.js
+++ latihan1.js
@@ -2,8 +2,13 @@ let angka = [1,2,3,4,5,6,7,8,9,10];
 angka.forEach(num=>{
     console.log(num*2);
 });
+
+
 console.log(angka);
 
 
 let.hasil = angka.map(num=>num*2);
 console.log(hasil);
+
+// latihan abjad
+
---
+++ latihan2.js
@@ -0,0 +1,5 @@
+let huruf = ['a', 'b', 'c','d'];
+
+let hasil = huruf.map(huruf=>huruf + 'ba');
+
+console.log(hasil);
